cc012f0c433f48267b85583baeef40da82b117d5
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-03-15 11:30:41 -0500

    Add files via upload

---
+++ BegVar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BegVar
+{
+
+	public static void main (String[] args)
+	{
+
+		Scanner myScanObj = new Scanner(System.in);
+
+		System.out.println("Enter an integer:");
+		int num1 = myScanObj.nextInt();
+
+		System.out.println("Hello World==" + num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+}
